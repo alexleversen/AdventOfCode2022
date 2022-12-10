1dabd5bbdc8a6aaba7d5a23d3a38a81d9afc48cb
Alex Leversen <adleversen@sep.com>
2022-12-10 15:12:23 -0500

    Implements more efficient solution to day 9 part 1

--- AdventOfCode2022/Day9Part1.cs
+++ AdventOfCode2022/Day9Part1.cs
@@ -1,12 +1,10 @@
-using System.Text;
 using System.Text.RegularExpressions;
+using AdventOfCode2022.Models.Day9;
 
 namespace AdventOfCode2022;
 
 public class Day9Part1 : IPuzzleSolver
 {
-    private const int SimulationWidth = 1000;
-    private const int SimulationHeight = 1000;
     private const string Right = "R";
     private const string Left = "L";
     private const string Up = "U";
@@ -14,125 +12,64 @@ public class Day9Part1 : IPuzzleSolver
 
     public string Run(string[] input)
     {
-        var ropeSimulation = new char[SimulationHeight][];
-        var visitedLocations = new List<(int row, int col)>();
-        for (var i = 0; i < SimulationHeight; i++)
+        var currentState = new RopeSimulation
         {
-            ropeSimulation[i] = new char[SimulationWidth];
-            Array.Fill(ropeSimulation[i], '.');
-        }
+            Head = (0, 0),
+            Tail = (0, 0)
+        };
+        var visitedLocations = new List<(int row, int col)>
+        {
+            (0, 0)
+        };
 
-        ropeSimulation[SimulationHeight / 2][SimulationWidth / 2] = 'H';
-        // Console.WriteLine("Initial state");
-        // Console.WriteLine(ToString(ropeSimulation));
 
         foreach (var line in input)
         {
-            Console.WriteLine($"Command: {line}");
-            var groups = new Regex("(\\w) (\\d)").Match(line).Groups;
+            var groups = new Regex("(\\w) (\\d+)").Match(line).Groups;
             var direction = groups[1].Value;
             var count = int.Parse(groups[2].Value);
             for (var i = 0; i < count; i++)
             {
-                // Console.WriteLine("Moving head...");
-                MoveHead(ropeSimulation, direction);
-                // Console.WriteLine(ToString(ropeSimulation));
-                // Console.WriteLine("Moving tail...");
-                visitedLocations.Add(ComputeAndMoveTail(ropeSimulation));
-                // Console.WriteLine(ToString(ropeSimulation));
+                MoveHead(currentState, direction);
+                var tailLocation = ComputeAndMoveTail(currentState);
+                if (!visitedLocations.Contains(tailLocation))
+                {
+                    visitedLocations.Add(tailLocation);
+                }
             }
         }
 
-        var locationCount = visitedLocations.Distinct().Count();
+        var locationCount = visitedLocations.Count;
 
         return $"{locationCount}";
     }
 
-    private static string ToString(IEnumerable<char[]> state)
+    private static void MoveHead(RopeSimulation currentState, string direction)
     {
-        var sb = new StringBuilder();
-        foreach (var line in state)
-        {
-            foreach(var currentChar in line)
-            {
-                sb.Append(currentChar);
-                sb.Append('\t');
-            }
-
-            sb.AppendLine();
-        }
-
-        return sb.ToString();
-    }
-
-    private static (int row, int col) FindEnd(IReadOnlyList<char[]> currentState, char endChar)
-    {
-        var (row, col) = (-1, -1);
-        var endFound = false;
-        for (var i = 0; i < currentState.Count; i++)
-        {
-            for (var j = 0; j < currentState[i].Length; j++)
-            {
-                if (currentState[i][j] != endChar) continue;
-                (row, col) = (i, j);
-                endFound = true;
-                break;
-            }
-
-            if (endFound)
-            {
-                break;
-            }
-        }
-
-        if (endChar == 'T' && !endFound)
-        {
-            (row, col) = FindEnd(currentState, 'H');
-        }
-
-        return (row, col);
-    }
-
-    private static void MoveHead(IReadOnlyList<char[]> currentState, string direction)
-    {
-        var (row, col) = FindEnd(currentState, 'H');
-        var (tailRow, tailCol) = FindEnd(currentState, 'T');
-        var replacementChar = '.';
-        if (row == tailRow && col == tailCol)
-        {
-            replacementChar = 'T';
-        }
+        var (row, col) = currentState.Head;
         switch (direction)
         {
             case Left:
-                currentState[row][col - 1] = 'H';
+                currentState.Head = (row, col - 1);
                 break;
             case Right:
-                currentState[row][col + 1] = 'H';
+                currentState.Head = (row, col + 1);
                 break;
             case Up:
-                currentState[row - 1][col] = 'H';
+                currentState.Head = (row - 1, col);
                 break;
             case Down:
-                currentState[row + 1][col] = 'H';
+                currentState.Head = (row + 1, col);
                 break;
             default:
                 throw new InvalidOperationException("Movement not allowed");
         }
-
-        currentState[row][col] = replacementChar;
     }
 
-    private static void MoveTail(IReadOnlyList<char[]> currentState, int fromRow, int fromCol, int toRow, int toCol)
+    private static (int row, int col) ComputeAndMoveTail(RopeSimulation currentState)
     {
-        currentState[fromRow][fromCol] = '.';
-        currentState[toRow][toCol] = 'T';
-    }
-
-    private static (int row, int col) ComputeAndMoveTail(IReadOnlyList<char[]> currentState)
-    {
-        var (headRow, headCol) = FindEnd(currentState, 'H');
-        var (tailRow, tailCol) = FindEnd(currentState, 'T');
+        var (headRow, headCol) = currentState.Head;
+        var (tailRow, tailCol) = currentState.Tail;
         if (Math.Abs(headRow - tailRow) + Math.Abs(headCol - tailCol) <= 1 || Math.Abs(headRow - tailRow) == 1 && Math.Abs(headCol - tailCol) == 1)
         {
             return (tailRow, tailCol);
@@ -140,7 +77,7 @@ public class Day9Part1 : IPuzzleSolver
 
         var toRow = tailRow + Math.Sign(headRow - tailRow);
         var toCol = tailCol + Math.Sign(headCol - tailCol);
-        MoveTail(currentState, tailRow, tailCol, toRow, toCol);
+        currentState.Tail = (toRow, toCol);
 
         return (toRow, toCol);
     }
---
+++ AdventOfCode2022/Models/Day9/RopeSimulation.cs
@@ -0,0 +1,7 @@
+namespace AdventOfCode2022.Models.Day9;
+
+public class RopeSimulation
+{
+    public (int row, int col) Head { get; set; }
+    public (int row, int col) Tail { get; set; }
+}
